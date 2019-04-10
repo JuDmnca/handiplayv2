7858229249198ee1c71c169bfad8b289724b7f29
JuDmnca <julie.demendonca@mmibordeaux.com>
2019-04-10 23:28:41 +0200

    fusion 1

---
+++ fusion 1/Assets/AsteroidBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehavior : MonoBehaviour
+{
+    public float m_speed = 200f;
+    public Rigidbody2D m_rb2D;
+    public float m_chrono = 0f;
+    public float Number;
+    public GameObject m_pos1;
+    public GameObject m_pos2;
+    public GameObject m_pos3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_chrono = 0f;
+
+        Number = Random.Range(0, 3);
+        if (Number == 1)
+        {
+            transform.position = m_pos1.transform.position;
+        }
+
+        if (Number == 2)
+        {
+            transform.position = m_pos2.transform.position;
+        }
+
+        if (Number == 0)
+        {
+            transform.position = m_pos3.transform.position;
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_chrono = m_chrono + Time.fixedDeltaTime;
+        transform.position += Vector3.down * m_speed * Time.fixedDeltaTime;
+
+
+    }
+}
+
---
+++ fusion 1/Assets/ItiBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItiBehavior : MonoBehaviour
+{
+	public Rigidbody2D m_rb2D;
+	public float m_vitesse = 1000f;
+
+	public GameObject m_gauche;
+	public GameObject m_droite;
+	public GameObject m_center;
+
+	public bool isLeft = false;
+	public bool isRight = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+	void FixedUpdate()
+	{
+
+		if (Input.GetAxis("Horizontal") < 0f) {
+		 	if (isLeft == false) {
+		 		if (isRight == true) {
+	 			transform.position = m_center.transform.position;
+	 		 	isRight = false;
+		 		}
+		 		else {
+		 		 transform.position = m_gauche.transform.position;
+		 		 isLeft = true;
+		 		}
+		 	}	
+		 }
+
+		 if (Input.GetAxis("Horizontal") > 0f) {
+		 	if (isRight == false) {
+		 		if (isLeft == true) {
+	 			transform.position = m_center.transform.position;
+	 		 	isLeft = false;
+		 		}
+		 		else {
+		 		 transform.position = m_droite.transform.position;
+		 		 isRight = true;
+		 		}
+		 	}	
+		 }
+	}
+}
